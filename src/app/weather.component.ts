5fafc52a2fc3183f5eacffa7fa98ece044cd03d9
radeeb bashir <radeebbashir@gmail.com>
2017-06-26 19:42:53 -0500

    fixed the service

---
+++ src/app/weather.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireAuth} from 'angularfire2/auth';
+import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
+import {firebaseConfig} from './app.module';
+import { WeatherService} from './weather.service';
+import { Router} from '@angular/router';
+import {Injectable} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+
+@Injectable()
+export class WeatherComponent implements OnInit {
+  city = "Loading";
+  temp = "Loading";
+  conditions = "Loading";
+  zip1;
+
+  constructor(private dataService:WeatherService){}
+
+  ngOnInit() {
+      this.getWeatherInfo();
+  }
+
+  getWeatherInfo(){
+      let url = 'http://api.openweathermap.org/data/2.5/weather?zip=65802' +
+      //this.zipCode +
+      '&appid=b86dc3179d853356b32565e7717caca5'+
+      '&units=imperial';
+      fetch(url)
+      .then(function(response){
+          return response.json();
+      }).then(function(json){
+          console.log(json);
+          this.city = json.name;
+          this.conditions = json.weather[0].description;
+          //format the first letter to upper case
+          this.conditions = this.conditions.charAt(0).toUpperCase() + this.conditions.slice(1);
+          this.temp = json.main.temp + String.fromCharCode(176) + ' F';
+      }.bind(this));
+  }
+}
